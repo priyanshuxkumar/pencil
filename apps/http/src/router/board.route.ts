995e6319736348ed5f0d0f2ec1e9cd6fa4a316cd
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-01-23 12:49:50 +0530

    fix create-board route

--- apps/http/src/router/board.route.ts
+++ apps/http/src/router/board.route.ts
@@ -1,14 +1,17 @@
 import { Router , Response , Request} from "express";
 import prisma from "db";
+import { authUserMiddleware } from "../middleware/user.middleware";
 
 const router = Router();
 
-router.post('/create' , async (req : Request , res : Response) => {
+router.post('/create' , authUserMiddleware , async (req : Request , res : Response) => {
+    const userId = req.id as number;
     try {
         const {name} = req.body;
         const board  = await prisma.board.create({
             data: {
-                name: name
+                name,
+                userId
             }
         })
         res.status(200).json(board)
