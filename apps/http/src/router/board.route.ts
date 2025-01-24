d2c4817f265bd4ab218cdf8ad5085f7a4aa3b911
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-01-24 14:24:26 +0530

    add generate-collab-link route

--- apps/http/src/router/board.route.ts
+++ apps/http/src/router/board.route.ts
@@ -1,5 +1,6 @@
 import { Router , Response , Request} from "express";
 import prisma from "db";
+import { v4 as uuidv4 } from 'uuid';
 import { authUserMiddleware } from "../middleware/user.middleware";
 
 const router = Router();
@@ -17,6 +18,16 @@ router.post('/create' , authUserMiddleware , async (req : Request , res : Respon
         res.status(200).json(board)
     } catch (error) {
         console.log("Error occured while creating board" , error)
+    }
+})
+
+router.get('/generate-collab-link' , async(req: Request , res: Response) => {
+    try {
+        const id = uuidv4();
+        const url = `${process.env.CLIENT_URL}?room=${id}`
+        res.status(200).json(url);
+    } catch (error) {
+        console.log("Error occured while generate link" , error)
     }
 })
 
